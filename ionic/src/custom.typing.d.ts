929bf7067b3976fa30ee5a5902bded48901138d0
rmadulla <maddula.rajarahul@gmail.com>
2020-06-24 15:49:25 +0530

    Changing imports

--- ionic/src/custom.typing.d.ts
+++ ionic/src/custom.typing.d.ts
@@ -1,2 +1,2 @@
 /// <reference path="../plugins/cordova-plugin-mfp/typings/worklight.d.ts" />
-/// <reference path="../plugins/cordova-plugin-mfp-push/typings/MFPPush.d.ts" />
+/// <reference path="../plugins/cordova-plugin-mfp-push/typings/mfppush.d.ts" />
